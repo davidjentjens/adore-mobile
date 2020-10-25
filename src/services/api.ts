65531075ab37dc287e77c326ad0f13773f4fbe69
David Maisonnette Jentjens <davidjentjens@gmail.com>
2020-10-25 18:00:16 -0300

    Prepare for testing

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://prepartyserver.me/',
+  // baseURL: 'https://prepartyserver.me/',
   // baseURL: 'http://142.93.48.40',
-  // baseURL: 'http://192.168.100.28:3333',
+  baseURL: 'http://192.168.0.23:3333',
 });
 
 export default api;
